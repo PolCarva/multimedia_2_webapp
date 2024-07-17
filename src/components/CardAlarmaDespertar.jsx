eff35d43a7522abcb75183812f72f1a0f60662b6
PolCarva <pablocarvalhogimenez@gmail.com>
2024-07-17 14:50:18 -0300

    chore: Update time format for 'despertar' and add periodic update for time remaining in CardHorario component

--- src/components/CardAlarmaDespertar.jsx
+++ src/components/CardAlarmaDespertar.jsx
@@ -48,7 +48,7 @@ const CardAlarmaDespertar = ({ setConfig, config }) => {
           </div>
 
           <div className="flex-1 flex flex-col gap-3">
-            <label className="flex mt-2 flex-col text-sm font-medium text-white">
+            {/* <label className="flex mt-2 flex-col text-sm font-medium text-white">
               <span className="-mb-4">Volumen</span>
               <Slider
                 onChange={(e) => handleChange(e, "volumen")}
@@ -62,7 +62,7 @@ const CardAlarmaDespertar = ({ setConfig, config }) => {
                 className="!h-3"
                 color="white"
               />
-            </label>
+            </label> */}
 
             <label
               htmlFor="sonido_tipo"
